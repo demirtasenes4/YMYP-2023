f5990066ceacae74737b4da05d2ad2d4a0d50674
vatan <vatan@QUADESH>
2023-08-01 19:36:39 +0300

    Add project files.

---
+++ 03.Operatörler/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//Operatörler
+//= eşittir operatörü => değer ataması yapar
+//== kontrol operatörü => a ve b değerini kontrol eder
+
+int a = 0;
+int b = 1;
+
+var c = (a == b);
+
+// + - * / Matematiksel operatörler
+
+int r = ((5 * 2) + 5 - 0);
+
+int z = 1;
+
+z += 5;
+
+int x = 1;
+
+x++;
+x--;
+
+//+ operatörü string değerlerde birleştirme işlemi yapar;
+
+string name = "Enes" + " " + "Demirtas"; // Enes Demirtaş
+string name2 = $"{name} 27 yaşında bir mühendis"; // ==> Enes Demirtaş 27 yaşında bir eğitmen
+
+//@ operatörü \ ters bölü işaretini kullanmamızı sağlayan operatör
+string url = @"http:\";
+ 
+//% => mod operatörü sayının verilen sayıya tam bölünüp bölünmediğini kontrol eder 0 veya küsürat dönüyor
+
+var r2 = (x % 2);
+
---
+++ 04.SartBloklari/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.ComponentModel.Design;
+
+
+Console.WriteLine("Hello, World!");
+
+
+//if bloğu
+//else if bloğu
+//switch bloğu
+
+int a = 1;
+int b = 3;
+
+if (a < 3)
+{ //scope
+    Console.WriteLine("a değeri 3'ten küçük bir değer!");
+}
+else if (a > 3)
+{
+    Console.WriteLine("a değeri 3'ten daha büyük bir değer");
+}
+else
+{
+    Console.WriteLine("a değeri 3'e eşittir");
+}
+
+if (b < 3)
+{ //scope
+    Console.WriteLine("b değeri 3'ten küçük bir değer!");
+}
+else if (b > 3)
+{
+    Console.WriteLine("b değeri 3'ten daha büyük bir değer");
+}
+else
+{
+    Console.WriteLine("b değeri 3'e eşittir");
+}
+
+    //syntax => bir kodun nasıl yazılması gerektiğidir
+    //&& => ve operatörü
+    //|| => veya operatörü
+
+    //if (b == 3 && a < 3 || a == 3 || a > 3) ;
+    //{
+    //Console.WriteLine("Olumlu!");
+    //}
+    //else
+    //{
+    //Console.WriteLine("Olumsuz :(");
+    //}
+
+    int kdv = 1;
+
+switch (kdv)
+{
+    case 1:
+        //işlem
+        Console.WriteLine("Ohh Çohşühür");
+        break;
+    case 10:
+        //işlem2
+        Console.WriteLine("Reyis sağolsun");
+        break;
+    case 20:
+        //işlem3
+        Console.WriteLine("Yuh amk");
+        break;
+}
