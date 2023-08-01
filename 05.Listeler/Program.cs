ce43b8645f54ff23726573afcda109c7000e634a
Quadesh <77413354+demirtasenes4@users.noreply.github.com>
2023-08-01 20:50:58 +0300

    Update 20:50

--- 05.Listeler/Program.cs
+++ 05.Listeler/Program.cs
@@ -1,2 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+//[ ] => listeleri temsil eder => arra işareti
+
+//string[] names = { "araba", "armut" };
+//index numarası dersek => 0'dan başlar
+//string[] names = new string[3];
+
+//names[0] = "Enes";
+//names[1] = "Enes";
+//names[2] = "Enes";
+
+//List<string> names = new List<string>(); { "Armut", "Elma" };
+
+List<string> names = new(); //örneğe dönüştürmek için new kelimesini kullanıyoruz. Örneğe dönüştürme işlemine yazılımda instance üretme deniyor.
+
+names.Add("Enes");
+names.Add("Enes");
+names.Add("Enes");
+names.Add("Enes");
+names.Add("Enes");
+names.Add("Enes");
+
+names[3] = "Enes'in günlüğü";
+
+Console.WriteLine(names[3]);
---
+++ 06.Donguler/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace _06.Donguler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            List<string> names = new();
+            names.Add("BTC");
+            names.Add("ETH");
+            names.Add("AVAX");
+            names.Add("VET");
+            names.Add("BAT");
+            names.Add("FET");
+            names.Add("XLM");
+
+            //Console.WriteLine(name[0]);
+            //Console.WriteLine(name[1]);
+            //Console.WriteLine(name[2]);
+            //Console.WriteLine(name[3]);
+            //Console.WriteLine(name[4]);
+            //Console.WriteLine(name[5]);
+
+            //for, foreach
+
+            for (int x = 0; x < names.Count(); x++)
+            {
+                if(x == 4)
+                {
+                    break;
+                }
+                if(x == 0)
+                {
+                    continue;
+                }
+                if(x == 3)
+                {
+                    continue;
+                }
+                Console.WriteLine(names[x]);
+            }
+
+            //foreach (var x in names)
+            //{
+            //    Console.WriteLine(x);
+            //}
+
+        }
+    }
+}
