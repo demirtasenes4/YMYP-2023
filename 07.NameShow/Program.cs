3e574f2cbbe05ec1449cd88450705a81b14d515f
Quadesh <77413354+demirtasenes4@users.noreply.github.com>
2023-08-02 16:36:37 +0300

    Randomly generated number guess game v1

---
+++ 07.NameShow/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.Design;
+
+namespace _07.NameShow
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.Write("Lütfen isminizi giriniz: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hoşgeldin " + name + "!");
+
+            Random random = new Random();
+            int randomNumber = random.Next(1, 11); // Generates a random number between 1 and 10 (inclusive).
+
+            while (true)
+            {
+
+                Console.WriteLine("Lütfen 1 ile 10 arasında bir sayı gir: ");
+
+                string number = Console.ReadLine();
+                int number1 = int.Parse(number);
+
+
+                if (randomNumber == number1)
+                {
+                    Console.WriteLine("Contratulations, you guessed right!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Unfortunately wrong, try again!");
+                }
+            }
+
+        
+        }
+    }
+}
